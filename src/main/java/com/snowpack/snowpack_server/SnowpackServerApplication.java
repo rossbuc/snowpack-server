15edb866d1faa37393d23db04f71de68078df304
Ross Buchan <rossbuchan1@googlemail.com>
2024-06-04 09:55:44 +0100

    Initial commit

---
+++ src/main/java/com/snowpack/snowpack_server/SnowpackServerApplication.java
@@ -0,0 +1,13 @@
+package com.snowpack.snowpack_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowpackServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowpackServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowpack/snowpack_server/SnowpackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowpack.snowpack_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowpackServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
