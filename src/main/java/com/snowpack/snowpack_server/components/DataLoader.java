ef04dae3c7d358f2700e22816d9b02a156413552
Ross Buchan <rossbuchan1@googlemail.com>
2024-06-13 11:24:04 +0100

    Add posts to data loader

--- src/main/java/com/snowpack/snowpack_server/components/DataLoader.java
+++ src/main/java/com/snowpack/snowpack_server/components/DataLoader.java
@@ -1,6 +1,9 @@
 package com.snowpack.snowpack_server.components;
 
+import com.snowpack.snowpack_server.models.Aspect;
+import com.snowpack.snowpack_server.models.Post;
 import com.snowpack.snowpack_server.models.User;
+import com.snowpack.snowpack_server.repositories.PostRepository;
 import com.snowpack.snowpack_server.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -17,6 +20,9 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    PostRepository postRepository;
+
     public DataLoader() {
     }
 
@@ -30,5 +36,13 @@ public class DataLoader implements ApplicationRunner {
         userRepository.save(user1);
         userRepository.save(user2);
         userRepository.save(user3);
+
+        Post post1 = new Post(45.555, -0.999, "some silly descriptions yea boi", 6790, Aspect.NE, 20, user1);
+        Post post2 = new Post(5.555, -10.999, "some silly even more silly descriptions yea boi", 690, Aspect.N, 0, user1);
+        Post post3 = new Post(40.555, 40.999, "some silly descriptions in a different place but by the same dood yea boi", 6790, Aspect.S, 3, user1);
+
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
     }
 }
