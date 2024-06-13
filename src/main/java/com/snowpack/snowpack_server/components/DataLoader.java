84aeb10e670971ff1c084413c982dd525a034d8a
Ross Buchan <rossbuchan1@googlemail.com>
2024-06-13 14:01:14 +0100

    Update data loader to give more posts and users

--- src/main/java/com/snowpack/snowpack_server/components/DataLoader.java
+++ src/main/java/com/snowpack/snowpack_server/components/DataLoader.java
@@ -11,10 +11,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
 
-//Comment out @Component to avoid running the DataLoader
 @Profile("!test")
-//@Component
+@Component
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
@@ -27,22 +27,47 @@ public class DataLoader implements ApplicationRunner {
     }
 
     public void run(ApplicationArguments arga) {
-//        Create mock users
+        // Create mock users
         User user1 = new User("rossbuc", "123456", "rossbuchan@duttycoding.sick");
         User user2 = new User("hamishrendick", "23456h", "hamish@hdog.net");
         User user3 = new User("sickskir", "4856ht", "shredsauce.shred");
+        User user4 = new User("janedoe", "password", "janedoe@example.com");
+        User user5 = new User("johndoe", "password1", "johndoe@example.com");
 
-//        Add users to user repository
+        // Add users to user repository
         userRepository.save(user1);
         userRepository.save(user2);
         userRepository.save(user3);
+        userRepository.save(user4);
+        userRepository.save(user5);
 
-        Post post1 = new Post(45.555, -0.999, "some silly descriptions yea boi", 6790, Aspect.NE, 20, user1);
-        Post post2 = new Post(5.555, -10.999, "some silly even more silly descriptions yea boi", 690, Aspect.N, 0, user1);
-        Post post3 = new Post(40.555, 40.999, "some silly descriptions in a different place but by the same dood yea boi", 6790, Aspect.S, 3, user1);
+        // Create random posts
+        Random random = new Random();
+        Aspect[] aspects = Aspect.values();
+        String[] descriptions = {
+                "Amazing snow conditions today!",
+                "Watch out for icy patches.",
+                "Perfect weather for skiing.",
+                "Fresh powder, it's a dream!",
+                "Visibility is low, be careful.",
+                "Snow is melting fast, slushy conditions.",
+                "Great day on the slopes!",
+                "Windy and cold, dress warm!",
+                "Early morning runs are the best!",
+                "Afternoon sun makes for perfect conditions."
+        };
 
-        postRepository.save(post1);
-        postRepository.save(post2);
-        postRepository.save(post3);
+        for (int i = 0; i < 200; i++) {
+            double latitude = -90 + (90 - (-90)) * random.nextDouble();
+            double longitude = -180 + (180 - (-180)) * random.nextDouble();
+            String description = descriptions[random.nextInt(descriptions.length)] + " Post number " + (i + 1);
+            int elevation = random.nextInt(8000);
+            Aspect aspect = aspects[random.nextInt(aspects.length)];
+            int dangerLevel = random.nextInt(5);
+            User user = userRepository.findAll().get(random.nextInt((int) userRepository.count()));
+
+            Post post = new Post(latitude, longitude, description, elevation, aspect, dangerLevel, user);
+            postRepository.save(post);
+        }
     }
 }
