3c45c04971640cd9dc0932201c75cbb166be3ed0
Ross Buchan <rossbuchan1@googlemail.com>
2024-07-10 07:18:33 +0100

    Write post specifications and modify post repository

--- src/main/java/com/snowpack/snowpack_server/repositories/PostRepository.java
+++ src/main/java/com/snowpack/snowpack_server/repositories/PostRepository.java
@@ -3,12 +3,13 @@ package com.snowpack.snowpack_server.repositories;
 import com.snowpack.snowpack_server.models.Aspect;
 import com.snowpack.snowpack_server.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
 
     List<Post> findAllByOrderByDateTimeDesc();
 
--- src/test/java/com/snowpack/snowpack_server/specifications/PostSpecification.java
+++ src/test/java/com/snowpack/snowpack_server/specifications/PostSpecification.java
@@ -1,4 +1,32 @@
 package com.snowpack.snowpack_server.specifications;
 
+import com.snowpack.snowpack_server.models.Aspect;
+import com.snowpack.snowpack_server.models.Post;
+import org.springframework.data.jpa.domain.Specification;
+
 public class PostSpecification {
+
+    public static Specification<Post> hasElevationGreaterThanEqual(Integer elevation) {
+        return (root, query, criteriaBuilder) ->
+                elevation == null ? null : criteriaBuilder.greaterThanOrEqualTo(root.get("elevation"), elevation);
+    }
+
+    public static Specification<Post> hasAspect(Aspect aspect) {
+        return (root, query, criteriaBuilder) ->
+                aspect == null ? null : criteriaBuilder.equal(root.get("aspect"), aspect);
+    }
+
+    public static Specification<Post> hasTemperatureGreaterThanEqual(Integer temperature) {
+        return (root, query, criteriaBuilder) ->
+                temperature == null ? null : criteriaBuilder.greaterThanOrEqualTo(root.get("temperature"), temperature);
+    }
+
+    public static Specification<Post> isSortedByDateTimeDesc(String sortBy) {
+        return (root, query, criteriaBuilder) -> {
+            if ("date".equals(sortBy)) {
+                query.orderBy(criteriaBuilder.desc(root.get("dateTime")));
+            }
+            return null;
+        };
+    }
 }
